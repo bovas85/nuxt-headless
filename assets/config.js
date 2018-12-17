c73041e541745471621164e40ef412ca2e0fdca1
Alessandro Giordo <16958684+bovas85@users.noreply.github.com>
2018-12-17 07:46:31 +0000

    Removed unneeded details, added comments

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// configure this file following the example below. 
+// these are the rest api endpoints and your wordpress url 
 const Config = {
   appTitle: 'Nuxt Headless',
   appTitleShort: 'Nuxt-headless',
@@ -7,15 +9,12 @@ const Config = {
   appThemeColor: '#ffffff ',
   appBgColor: '#00172c ',
   appIcon: 'assets/app-icon.png',
-  wpDomain: 'https://api.wordpress_site.com/wp-json',
-  client: 'https://api.wordpress_site.com',
-  url: 'http://your-url.com',
-  loadDbName: 'starter_wp',
+  wpDomain: 'https://[api.wordpress_site.com]/wp-json',
+  client: 'https://[api.wordpress_site.com]',
+  url: '[http://your-url.com]',
   api: {
-    homePage: '/wp/v2/pages/8/',
-    projects: '/wp/v2/casestudies',
-    caseStudy: '/wp/v2/casestudies?slug=',
-    postFormContact: '/contact-form-7/v1/contact-forms/{your_id}/feedback'
+    homePage: '/wp/v2/pages/[page_id]',
+    postFormContact: '/contact-form-7/v1/contact-forms/{form_id}/feedback'
   }
 }
 
